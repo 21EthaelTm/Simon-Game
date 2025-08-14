541f4dce1d44427b1f8fff0a9fb82f35bbbf99bf
Bilisuma Ambisa <21Bilisuma@gmail.com>
2025-08-14 20:49:46 +0300

    adds animation and audio

---
+++ mysimon.js
@@ -0,0 +1,42 @@
+const greenBox = $(".green");
+const yellowBox = $(".yellow");
+const blueBox = $(".blue");
+const redBox = $(".red");
+const gamedisplay = [];
+const playmemo = [];
+const arrybox = [greenBox, yellowBox, blueBox, redBox];
+$(document).on("keydown", function (event) {
+  let randnum = Math.floor(Math.random() * 4);
+  let element = arrybox[randnum];
+  element.addClass("spark");
+  setTimeout(function () {
+    element.removeClass("spark");
+  }, 300);
+  gamedisplay.push(arrybox[randnum]);
+  audiobox(randnum);
+});
+
+function audiobox(index) {
+  switch (index) {
+    case 0:
+      new Audio("sounds/green.mp3").play();
+
+      break;
+    case 1:
+      new Audio("sounds/blue.mp3").play();
+
+      break;
+    case 2:
+      new Audio("sounds/blue.mp3").play();
+
+      break;
+
+    case 3:
+      new Audio("sounds/blue.mp3").play();
+      break;
+
+    default:
+      return;
+  }
+}
+
